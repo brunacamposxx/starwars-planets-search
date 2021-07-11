59c8bea573fc8a52f005860853e773c07b310e14
Bruna Campos <92brunacampos@gmail.com>
2021-07-10 21:21:59 -0300

    requisito 4 COM BUG

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,7 +6,7 @@ function Table() {
   // requisito 1
   const [data, setData] = useState([]);
   const [titles, setTitles] = useState([]);
-  const [newData, setNewData] = useState([]);
+  const [newData, setNewData] = useState([]); // requisito 2
 
   useEffect(() => {
     const getPlanets = async () => {
@@ -15,14 +15,24 @@ function Table() {
         delete result.residents;
       });
       setData(results);
-      setNewData(data);
+      setNewData(data); // requisito 2
       setTitles(Object.keys(results[0]));
     };
     getPlanets();
   }, [data]);
 
   // requisito 2
-  const { setSearch, filters: { filterByName: { name } } } = useContext(MyContext); // pega para consumo a função criada no provider;
+  const { setColumnFilter,
+    setComparisonFilter,
+    setValueFilter,
+    setButtonFilter,
+    setSearch,
+    columnFilter,
+    comparisonFilter,
+    valueFilter,
+    // buttonFilter,
+    filters: { filterByName: { name } } } = useContext(MyContext); // pega para consumo a função criada no provider;
+
   function handleChange(event) {
     setSearch(event.target.value);
     const filterName = data.filter((planet) => planet.name.includes(event.target.value));
@@ -48,6 +58,27 @@ function Table() {
     </tr>
   )));
 
+  useEffect(() => {
+    if (comparisonFilter === 'maior que') {
+      const dadosnovos = data.filter(
+        (param) => parseFloat(param[`${columnFilter}`]) > parseFloat(`${valueFilter}`),
+      );
+      setNewData(dadosnovos);
+    } else if (comparisonFilter === 'menor que') {
+      const dadosnovos = data.filter(
+        (param) => parseFloat(param[`${columnFilter}`]) < parseFloat(`${valueFilter}`),
+      );
+      setNewData(dadosnovos);
+    } else if (comparisonFilter === 'igual a') {
+      const dadosnovos = data.filter(
+        (param) => parseFloat(param[`${columnFilter}`]) === parseFloat(`${valueFilter}`),
+      );
+      setNewData(dadosnovos);
+    } else {
+      setNewData([]);
+    }
+  }, [columnFilter, comparisonFilter, data, valueFilter]);
+
   return (
     <>
       <header>
@@ -61,6 +92,40 @@ function Table() {
           />
         </label>
       </header>
+      <div>
+        <select
+          name="select"
+          onChange={ (event) => { setColumnFilter(event.target.value); } }
+          data-testid="column-filter"
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select
+          name="select"
+          onChange={ (event) => { setComparisonFilter(event.target.value); } }
+          data-testid="comparison-filter"
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          onChange={ (event) => { setValueFilter(event.target.value); } }
+          data-testid="value-filter"
+        />
+        <button
+          type="submit"
+          onClick={ (event) => { setButtonFilter(event.target.value); } }
+          data-testid="button-filter"
+        >
+          Filter
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -4,14 +4,40 @@ import MyContext from './MyContext';
 
 function MyProvider({ children }) {
   const [search, setSearch] = useState('');
+  const [columnFilter, setColumnFilter] = useState([]); // requisito 3
+  const [comparisonFilter, setComparisonFilter] = useState([]); // requisito 3
+  const [valueFilter, setValueFilter] = useState([]); // requisito 3
+  const [buttonFilter, setButtonFilter] = useState([]); // requisito 3
+
   const filters = {
     filterByName: {
       name: search,
     },
+    filterByNumericValues: [
+      {
+        column: columnFilter,
+        comparison: comparisonFilter,
+        value: valueFilter,
+      },
+    ],
   };
 
   return (
-    <MyContext.Provider value={ { search, setSearch, filters } }>
+    <MyContext.Provider
+      value={ {
+        setColumnFilter,
+        setComparisonFilter,
+        setValueFilter,
+        setButtonFilter,
+        setSearch,
+        columnFilter,
+        comparisonFilter,
+        valueFilter,
+        buttonFilter,
+        search,
+        filters,
+      } }
+    >
       {children}
     </MyContext.Provider>
   );
